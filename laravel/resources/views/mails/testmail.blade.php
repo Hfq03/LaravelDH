e528e8c6a11fe5dd6ef1ad8a458c84e447f22f4a
hugofq <huflqu@fp.insjoaquimmir.cat>
2022-10-14 19:46:22 +0200

    Actualizacion

---
+++ laravel/resources/views/mails/testmail.blade.php
@@ -0,0 +1,21 @@
+<!-- <x-mail::message>
+# Introduction
+
+The body of your message.
+
+<x-mail::button :url="''">
+Button Text
+</x-mail::button> -->
+@component('mail::message')
+# Hello {{$content['name']}},
+ 
+{{$content['body']}}
+ 
+@component('mail::button', ['url' => $content['url']])
+Click Here
+@endcomponent
+ 
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
+<!-- </x-mail::message> -->
