1d5b864278bd81bf2666400b07a31b59d06fa434
hugofq <huflqu@fp.insjoaquimmir.cat>
2022-10-25 16:57:57 +0200

    8.2

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+    @section('content')
+    <h2>{{ __('Resources') }}</h2>
+    <a href="{{ url('/files') }}">{{ __('Files') }}</a>
+    @endsection
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
