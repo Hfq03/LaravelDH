41711d931e46755ad17ed773cbe885a0286d875b
hugofq <huflqu@fp.insjoaquimmir.cat>
2022-11-04 16:04:43 +0100

    9.3

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -36,4 +36,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('files', FileController::class);
+Route::resource('files', FileController::class)->middleware(['auth', 'role:2']);
