c71a1e7830f95cc4f1061c80a2431546775312c2
Hfq03 <huflqu@fp.insjoaquimmir.cat>
2022-11-28 00:06:04 +0100

    Falta likes

--- laravel/resources/views/post/edit.blade.php
+++ laravel/resources/views/post/edit.blade.php
@@ -12,41 +12,44 @@
                         @method('PUT')
                         <table class="table">
                             <thead>
-                                <tr>
-                                    <td scope="col">ID</td>
-                                    <td scope="col">{{ __('fields.body') }}</td>
-                                    <td scope="col">{{ __('fields.file') }}</td>
-                                    <td scope="col">{{ __('fields.latitude') }}</td>
-                                    <td scope="col">{{ __('fields.longitude') }}</td>
-                                    <td scope="col">{{ __('fields.visibility_id') }}</td>
-                                    <td scope="col">{{ __('fields.author') }}</td>
-                                    <td scope="col">Created</td>
-                                    <td scope="col">Updated</td>
-                                </tr>
+                            <div>
+                                <td>
+                                    <p>{{ __('fields.created_at') }}: {{ $post->created_at }}</p> 
+                                </td>
+                                <td>
+                                    <p class="derecha">{{ __('fields.updated_at') }}: {{ $post->updated_at }}</p>
+                                </td>
+                            <div>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>{{ $post->id }}</td>
-                                    <td>{{ $post->body }}</td>
-                                    <td><img class="img-fluid" src="{{ asset("storage/{$file->filepath}") }}" /></td>
-                                    <td>{{ $post->latitude }}</td>
-                                    <td>{{ $post->longitude }}</td>
-                                    <td>{{ $post->visibility_id }}</td>
-                                    <td>{{ $post->user->name }}</td>
-                                    <td>{{ $post->created_at }}</td>
-                                    <td>{{ $post->updated_at }}</td>
+                                    <td colspan="2">{{ $post->user->name }}</td>
+                                </tr>
+                                <tr>
+                                    @if($file->id == $post->file_id)
+                                        <td colspan="2"><img class="img" src="{{ asset("storage/{$file->filepath}") }}" /></td>
+                                    @endif
                                 </tr>
                             </tbody>
+                            <tr>
+                                <td colspan="2">{{ $post->user->name }}:&nbsp{{ $post->body }}</td>
+                            </tr>
                         </table>
                         <div class="form-group">
-                            <label for="body">{{ __('fields.body') }}</label><br>
-                            <input type="text" id="body" name="body"><br>
-                            <label for="latitude">{{ __('fields.latitude') }}</label><br>
-                            <input type="text" id="latitude" name="latitude"><br>
-                            <label for="longitude">{{ __('fields.longitude') }}</label><br>
-                            <input type="text" id="longitude" name="longitude"><br>
-                            <label for="visibility_id">{{ __('fields.visibility_id') }}</label><br>
-                            <input type="text" id="visibility_id" name="visibility_id"><br>
+                            <div class="eizquierda">
+                                <label for="body">{{ __('fields.body') }}</label>
+                                <input type="text" id="body" name="body">
+                                <label for="latitude">{{ __('fields.latitude') }}</label>
+                                <input type="text" id="latitude" name="latitude">
+                            </div>
+                            <br>
+                            <div class="ederecha">
+                                <label for="longitude">{{ __('fields.longitude') }}</label>
+                                <input type="text" id="longitude" name="longitude">
+                                <label for="visibility_id">{{ __('fields.visibility_id') }}</label>
+                                <input type="text" id="visibility_id" name="visibility_id">
+                            </div>
+                            <br>
                             <label for="upload">{{ __('fields.update') }}:</label>
                             <input type="file" class="form-control" name="upload"/>
                             <br>
